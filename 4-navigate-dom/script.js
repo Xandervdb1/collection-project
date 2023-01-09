f72fd9f412fa6b38b83afb9ebf9aecd95db15532
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-01-09 15:30:46 +0100

    Add solution for exercises chapter 4

--- 4-navigate-dom/script.js
+++ 4-navigate-dom/script.js
@@ -1,2 +1,22 @@
 // Select the last child of the <ol> tag and 
 // put it at the beginning of the list
+let ol = document.querySelector("ol");
+let lastChildOl = ol.lastElementChild;
+
+ol.prepend(lastChildOl);
+
+// Move the <h2> of the third section in the second one and vice-versa
+let main = document.querySelector("main");
+
+    //grabbing the heading for the second section
+let lastChildMain = main.lastElementChild;
+let secondHeading = lastChildMain.querySelector("h2");
+    //grabbing the heading for the last section
+let secondChildMain = main.children[1];
+let thirdHeading = secondChildMain.querySelector("h2");
+    //switching the headings around
+lastChildMain.querySelector("div").prepend(thirdHeading);
+secondChildMain.prepend(secondHeading);
+
+// Delete the last section from the DOM, we don't need it anyways
+lastChildMain.parentElement.removeChild(lastChildMain);
